dee034db9a8f350b99011842cf87ac676d3ec55c
Arjun Vachhani <info.arjunvachhani@gmail.com>
2024-05-15 17:10:20 +0000

    Update Message.cs

--- OrderMatcher/OrderMatcher.Types/Message.cs
+++ OrderMatcher/OrderMatcher.Types/Message.cs
@@ -5,20 +5,17 @@ namespace OrderMatcher.Types
     public readonly struct Message
     {
         private readonly MessageType _type;
-        private readonly ReadOnlyMemory<byte> _body;
         private readonly object _object;
         private readonly ulong _deliveryTag;
 
-        public Message(MessageType type, ReadOnlyMemory<byte> body, object obj, ulong deliveryTag)
+        public Message(MessageType type, object obj, ulong deliveryTag)
         {
             _type = type;
-            _body = body;
             _object = obj;
             _deliveryTag = deliveryTag;
         }
 
         public MessageType MessageType => _type;
-        public ReadOnlyMemory<byte> Bytes => _body;
         public object Object => _object;
         public ulong DeliveryTag => _deliveryTag;
     }
